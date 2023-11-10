6492b28a5e93aa2726b41a3dd0190623b36a582d
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-11-10 16:19:03 -0300

    feat: paginate

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -5,8 +5,9 @@ import { CreateInvoice } from '@/src/ui/invoices/buttons';
 import { lusitana } from '@/src/ui/fonts';
 import { InvoicesTableSkeleton } from '@/src/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/src/lib/data';
 
-export default function Invoices(
+export default async function Invoices(
   {
     searchParams,
   }: {
@@ -18,7 +19,8 @@ export default function Invoices(
 ) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
-  
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -32,7 +34,7 @@ export default function Invoices(
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   )
