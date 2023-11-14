87d1511185761bce0213457edf63be0661341d6c
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-11-14 09:44:35 -0300

    feat: update invoices

---
+++ src/app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,28 @@
+import Form from '@/src/ui/invoices/edit-form'
+import Breadcrumbs from '@/src/ui/invoices/breadcrumbs'
+import { fetchCustomers, fetchInvoiceById } from '@/src/lib/data'
+
+export default async function Edit({ params }: { params: { id: string } }) {
+  const id = params.id
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ])
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -20,7 +20,7 @@ export async function createInvoice(formData: FormData) {
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  
+
   })
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
@@ -32,4 +32,26 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
+}
+
+const UpdateInvoice = InvoicesSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
