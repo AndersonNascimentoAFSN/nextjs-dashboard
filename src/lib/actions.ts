9e461c12cd1be281272d406a141a09be128f8689
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-11-16 11:23:56 -0300

    feat: add authenticate

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,6 +4,7 @@ import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { z } from 'zod'
+import { signIn } from '@/auth';
 
 const InvoicesSchema = z.object({
   id: z.string(),
@@ -101,7 +102,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice')
+  // throw new Error('Failed to Delete Invoice')
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
@@ -111,5 +112,19 @@ export async function deleteInvoice(id: string) {
     return {
       message: 'Database error: Failed to Delete Invoice.',
     }
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin';
+    }
+    throw error;
   }
 }
